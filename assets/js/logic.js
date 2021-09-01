d131f580e45e2bbe5944bde6d39cc3e28bef392a
Zach B <nativeblanks@gmail.com>
2021-09-01 18:56:09 -0400

    fixing referencing the assets folder

---
+++ assets/js/logic.js
@@ -0,0 +1,11 @@
+// variables
+
+var saveBtn = document.getElementById("saveBtn")
+var userInput = document.getElementById("userInput")
+
+
+saveBtn.addEventListener('click',saveInput)
+
+function saveInput() {
+    
+}
